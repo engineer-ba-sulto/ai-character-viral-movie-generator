f7bf6e2db1fe8a589afc725720c9259623b6594c
Hiroki Chiyoda <contact@arafipro.com>
2025-10-08 07:41:04 +0900

    T016-create-login-page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-center mb-6">ログイン</h2>
+      <LoginForm />
+    </div>
+  );
+}
